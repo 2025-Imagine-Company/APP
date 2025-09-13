1d53bf8670226803abf7c548834a14054a17e9f2
LeeYoonJong <l2yoonjong@gmail.com>
2025-09-13 16:30:21 +0900

    feat: intro 화면 구현

---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, '/login');
+          },
+          child: const Text('Click to get start'),
+        ),
+      ),
+    );
+  }
+}
