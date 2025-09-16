5498c857f38eaa0e56523be03180228f3fe9b42a
chaery0ung <chxerxy@naver.com>
2025-09-16 19:33:35 +0900

    feat: 마이페이지 UI 바텀바 위젯
    # Conflicts:
    #	lib/main.dart

---
+++ lib/core/widgets/custom_bottom_bar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomBar extends StatelessWidget {
+  const CustomBottomBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFFE9E9E9),
+      width: 351,
+      height: 69,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(34),
+            color: Color(0xFFD9D9D9),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.home_filled, color: Colors.black, size: 24,),
+              Icon(Icons.mic, color: Colors.black, size: 26,),
+              CircleAvatar(
+                  radius: 15,
+                  backgroundColor: Color(0xFFFF2424),
+                  child: const Icon(Icons.currency_bitcoin_outlined,color: Color(0xFFD9D9D9),)
+              ),
+              Icon(Icons.settings, color: Colors.black, size: 26,)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
